526f85aec552da493b1f6d055ade361f90a117ac
Konovalo-AP <sanches-zi@mail.ru>
2021-02-02 00:41:14 +0300

    add test WhenSumIsNegative_ThenThrowException

--- src/test/java/ru/netology/cashbackhacker/unit/CashbackServiceTest.java
+++ src/test/java/ru/netology/cashbackhacker/unit/CashbackServiceTest.java
@@ -1,14 +1,25 @@
 package ru.netology.cashbackhacker.unit;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.cashbackhacker.CashbackService;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CashbackServiceTest {
 
     private CashbackService service;
+    private static Random random;
+    final static int AMOUNT_SPENT_FOR_CASHBACK = 1000;
+
+    @BeforeAll
+    public static void initialize(){
+        random = new Random();
+    }
 
     @BeforeEach
     public void setUp() {
@@ -17,7 +28,7 @@ public class CashbackServiceTest {
 
     @Test
     public void WhenSumMultipleOfThousand_ThenNoRemain() {
-        int sum = 1000;
+        int sum = AMOUNT_SPENT_FOR_CASHBACK * (random.nextInt(100) + 1);
         int remain = service.remain(sum);
         int expectedRemain = 0;
         assertEquals(expectedRemain, remain);
@@ -25,9 +36,14 @@ public class CashbackServiceTest {
 
     @Test
     public void WhenSumNotMultipleOfThousand_ThenNeedRemain() {
-        int sum = 300;
+        int sum = AMOUNT_SPENT_FOR_CASHBACK + (random.nextInt(998) + 1);
         int remain = service.remain(sum);
-        int expectedRemain = 1000 - sum;
+        int expectedRemain =  AMOUNT_SPENT_FOR_CASHBACK - sum % AMOUNT_SPENT_FOR_CASHBACK;
         assertEquals(expectedRemain, remain);
+    }
+
+    @Test
+    public void WhenSumIsNegative_ThenThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> service.remain(-1000));
     }
 }
