6147ad3d941817326990c24d21ac3aa7d53e7442
Konovalo-AP <sanches-zi@mail.ru>
2021-02-02 00:14:14 +0300

    add test WhenSumNotMultipleOfThousand_ThenNeedRemain

--- src/test/java/ru/netology/cashbackhacker/unit/CashbackServiceTest.java
+++ src/test/java/ru/netology/cashbackhacker/unit/CashbackServiceTest.java
@@ -1,23 +1,14 @@
 package ru.netology.cashbackhacker.unit;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.cashbackhacker.CashbackService;
-
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashbackServiceTest {
 
     private CashbackService service;
-    private static Random random;
-
-    @BeforeAll
-    public static void initialize() {
-        random = new Random();
-    }
 
     @BeforeEach
     public void setUp() {
@@ -25,9 +16,18 @@ public class CashbackServiceTest {
     }
 
     @Test
-    public void WhenSumMultipleOf1000_ThenNoRemain() {
-        int sum = (random.nextInt(100) + 1) * 1000;
+    public void WhenSumMultipleOfThousand_ThenNoRemain() {
+        int sum = 1000;
         int remain = service.remain(sum);
-        assertEquals(0, remain);
+        int expectedRemain = 0;
+        assertEquals(expectedRemain, remain);
+    }
+
+    @Test
+    public void WhenSumNotMultipleOfThousand_ThenNeedRemain() {
+        int sum = 300;
+        int remain = service.remain(sum);
+        int expectedRemain = 1000 - sum;
+        assertEquals(expectedRemain, remain);
     }
 }
