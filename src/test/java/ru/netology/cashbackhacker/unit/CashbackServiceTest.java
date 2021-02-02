706ccb3f1de373159156d3c927115eae917c5dd3
Konovalo-AP <sanches-zi@mail.ru>
2021-02-02 21:24:26 +0300

    refactoring tests

--- src/test/java/ru/netology/cashbackhacker/unit/CashbackServiceTest.java
+++ src/test/java/ru/netology/cashbackhacker/unit/CashbackServiceTest.java
@@ -1,26 +1,17 @@
 package ru.netology.cashbackhacker.unit;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.cashbackhacker.CashbackService;
 
-import java.util.Random;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CashbackServiceTest {
 
-    private CashbackService service;
-    private static Random random;
-
-    @BeforeAll
-    public static void initialize() {
-        random = new Random();
-    }
+    protected CashbackService service;
 
     @BeforeEach
     public void setUp() {
@@ -29,7 +20,7 @@ public class CashbackServiceTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/multipleOfThousandSums.csv", numLinesToSkip = 1)
-    public void WhenSumMultipleOfThousand_ThenNoRemain(int sum) {
+    public void whenSumMultipleOfThousand_ThenNoRemain(int sum) {
         int remain = service.remain(sum);
         int expectedRemain = 0;
         assertEquals(expectedRemain, remain);
@@ -37,14 +28,18 @@ public class CashbackServiceTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/notMultipleOfThousandSums.csv", numLinesToSkip = 1)
-    public void WhenSumNotMultipleOfThousand_ThenNeedRemain(int sum, int expectedRemain) {
+    public void whenSumNotMultipleOfThousand_ThenNeedRemain(int sum, int expectedRemain) {
         int remain = service.remain(sum);
         assertEquals(expectedRemain, remain);
     }
 
     @Test
-    public void WhenSumIsNegative_ThenThrowException() {
+    public void whenSumIsNegative_ThenThrowException() {
         int sum = -1000;
-        assertThrows(IllegalArgumentException.class, () -> service.remain(sum));
+        String expectedExceptionMessage = "amount must be greater than zero";
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> service.remain(sum));
+        assertEquals(expectedExceptionMessage, thrown.getMessage());
     }
 }
